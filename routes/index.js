376b3e7a810b9373fa96f1affa61bfcaf460caed
Ernest Bruno <erndobruno@gmail.com>
2021-12-23 12:34:08 -0800

    Add index route and post route for adding to do to the list.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var todosList = require('../models/todos');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {todosList});
 });
+
+router.post('/todos', function (req, res) {
+  req.body.done = false;
+  todosList.push(req.body);
+  res.redirect('/')
+})
 
 module.exports = router;
